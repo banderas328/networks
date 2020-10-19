69608be8ad11c4a7f3777e5bb6795c3d2daa3be8
anton <banderas328@yandex.ru>
2020-10-19 23:37:30 +0300

    test

---
+++ config/autoload/global.php
@@ -0,0 +1,14 @@
+<?php 
+return array(
+    'webhost'  => 'http://networks.local/',
+    'database' => array(
+        'driver' => 'Pdo',
+        'dsn'    => 'mysql:dbname=networks;host=localhost;charset=utf8',
+        'params'  => array(
+            'host'     => 'localhost',
+            'username' => 'root',
+            'password' => 'root',
+            'dbname'   => 'networks'
+        )
+    )
+);
