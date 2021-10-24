519a51a3a5ebaed361a4d28cdd164d509754acda
banderas328@yandex.ru <banderas328@gmail.com>
2021-10-24 20:06:18 +0300

    update and fixes

--- module/Tasks/src/Tasks/Controller/BoardsController.php
+++ module/Tasks/src/Tasks/Controller/BoardsController.php
@@ -20,7 +20,7 @@ class BoardsController extends Controller\preloaderController
         $request = $this->getRequest();
         if ($request->isPost()) {
             $boards =  $this->getBoardsTable()->getProjectBoards($request);
-            return @array('boards' => $boards,"project_id" => $request->getPost()->project_id);
+            return @array('boards' => $boards);
         }
         return false;
     }
