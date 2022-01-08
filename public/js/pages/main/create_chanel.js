0f2386f6431b53c9b544921985c005962cbc67e2
banderas328@yandex.ru <banderas328@gmail.com>
2022-01-08 07:58:56 +0300

    private chanels fix

--- public/js/pages/main/create_chanel.js
+++ public/js/pages/main/create_chanel.js
@@ -1,20 +1,36 @@
+$('#create_chanel_submit').click(function () {
+    var name = $("#chanel_name").val();
+    var is_private = $("#is_private").is(':checked');
+    if (is_private) {
+        is_private = "1";
+    } else {
+        is_private = "0";
+    }
+    alert("chanel");
+    $.ajax({
+        url: "chanels/createChanel",
+        type: "POST",
+        data: {"chanel_name": name, "is_private": is_private},
+       // dataType: "json"
+    }).done(function (data) {
+        $.ajax({
+            type: "GET",
+            url: "chanels/indexPublic"
 
-    $('#create_chanel_submit').click(function(){
-       var name = $("#chanel_name").val();
-       var is_private = $("#is_private").is(':checked');
-       if(is_private) {is_private = "1";}
-       else {is_private = "0";}
-       $.ajax({
-    	   url: "chanels/createChanel",
-    	   type: "POST",
-    	   data: {"chanel_name":name,"is_private":is_private},
-    	 
-    	   dataType: "json"
-    	 }).done(function (data) {
-             alert(data);
-             console.info(data);
-         });;
+        })
+            .done(function (data) {
+                $("#chanels").html(data);
+            });
+        $.ajax({
+            type: "GET",
+            url: "chanels/indexPrivate"
+
+        })
+            .done(function (data) {
+                $("#private_chanels").html(data);
+            });
     });
+});
 
 
 
