6dee7b0550209d8c8b3939d6ac91ec089cf9867e
banderas328@yandex.ru <banderas328@gmail.com>
2022-06-08 19:43:11 +0300

    frames

--- module/Frames/src/Frames/Model/FramesTable.php
+++ module/Frames/src/Frames/Model/FramesTable.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Frames\Model;
+
 use Zend\InputFilter\Factory as InputFactory;
 use Zend\InputFilter\InputFilter;
 use Zend\InputFilter\InputFilterAwareInterface;
@@ -16,7 +18,7 @@ class FramesTable
 
     public function __construct()
     {
-        $config  =  new Config(Factory::fromFile('config/autoload/global.php'), true);
+        $config = new Config(Factory::fromFile('config/autoload/global.php'), true);
         $adapter = new \Zend\Db\Adapter\Adapter (array(
             'driver' => $config->database->driver,
             'dsn' => $config->database->dsn,
@@ -24,45 +26,51 @@ class FramesTable
             'username' => $config->database["params"]->username,
             'password' => $config->database["params"]->password,
         ));
-        $this->tableGateway = new \Zend\Db\TableGateway\TableGateway("frames",$adapter);
+        $this->tableGateway = new \Zend\Db\TableGateway\TableGateway("frames", $adapter);
         $this->adapter = $adapter;
     }
 
-    public function createFrame($request) {
+    public function createFrame($request)
+    {
         session_start();
         $user_session = $_SESSION['user'];
         $userId = $user_session["id"];
-        $url =  $request->getPost()->frame_url;
-        $image =  $request->getPost()->file;
-        $name =  $request->getPost()->frame_name;
+        $url = $request->getPost()->frame_url;
+        $image = $request->getPost()->file;
+        $name = $request->getPost()->frame_name;
         session_start();
         $user_session = $_SESSION['user'];
         $userId = $user_session["id"];
-        $path = "userfiles/".$userId;
+        $path = "userfiles/" . $userId;
         $fileName = uniqid();
         $file = $path . '/' . $userId . $fileName . $_FILES['file']['name'];
         move_uploaded_file($_FILES['file']['tmp_name'], getcwd() . "/public/" . $file);
-        $data = ["url" => $url ,"image" => $file,"name"=>$name,"user_id" => $userId];
+        $data = ["url" => $url, "image" => $file, "name" => $name, "user_id" => $userId];
         $this->tableGateway->insert($data);
         return true;
     }
 
-    public function getFrames(){
+    public function getFrames()
+    {
         session_start();
         $user_session = $_SESSION['user'];
         $userId = $user_session["id"];
-        $sql = "SELECT * FROM `frames`   WHERE frames.user_id='".$userId."'";
+        $sql = "SELECT * FROM `frames`   WHERE frames.user_id='" . $userId . "'";
         $resultSet = $this->adapter->query($sql, $this->adapter::QUERY_MODE_EXECUTE);
-        $frames =   $resultSet->toArray();
+        $frames = $resultSet->toArray();
         return $frames;
     }
 
-        public function daleteFrame($request){
-        $frame_id =  $request->getPost()->id;
-        $frame = $this->tableGateway->select(['id'=>$frame_id]);
-        var_dump($frame);die();
-        $this->tableGateway->delete(["id"=>$frame_id]);
-   }
+    public function daleteFrame($request)
+    {
+        $user_session = $_SESSION['user'];
+        $userId = $user_session["id"];
+        $frameId = $request->getPost()->frame_id;;
+        $frame = $this->tableGateway->select(['id' => $frameId, 'user_id' => $userId]);
+        $frameImage =  $frame->toArray()[0]["image"];
+        @unlink($_SERVER["DOCUMENT_ROOT"]."/".$frameImage);
+        $this->tableGateway->delete(["id" => $frameId]);
+    }
 
 
 }
