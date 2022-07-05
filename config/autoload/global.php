a789421076ac9dd703ddbef9945d15257443b881
Жаврид Антон <banderas328@gmail.com>
2022-07-05 12:51:22 +0300

    bugfix, website user restore

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,12 +14,12 @@ return array(
     'smtp' => array(
         'yandex' => array(
             'address' => 'smtp.yandex.ru',
-            'username' => 'banderas328@yandex.ru',
-            'password' => 'qxihegaoxyeeudqb',
+            'username' => 'xxxxxxxxxx@yandex.ru',   //yandex login
+            'password' => 'XXXXXXXXXXX',            //yandex application password
             'secure' => 'TLS',
             'port' => '587',
-            'from_mail' => 'banderas328@yandex.ru',
-            'from_name' => 'octopus admin'
+            'from_mail' => 'xxxxxxxxx@yandex.ru',   // from email
+            'from_name' => 'admin'                  // from name
         )
     )
 );
