8993d76da8a497b8268e7859619d4393267add0e
Жаврид Антон <banderas328@gmail.com>
2023-02-14 17:57:18 +0300

    some taskmanager ui fix

---
+++ database/migrations/admin.sql
@@ -0,0 +1,7 @@
+CREATE TABLE networks_08_2022.admins (
+                                         id int auto_increment NOT NULL,
+                                         user_id integer NOT NULL
+)
+    ENGINE=InnoDB
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_unicode_ci;
